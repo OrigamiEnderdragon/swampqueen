85cb636e286d847d05ff0982662cfb32806ad51f
Max Gilmour <mgilmour@maxgmr.ca>
2025-04-26 13:50:18 -0700

    Add dice tests

--- swampqueen-core/src/dice.rs
+++ swampqueen-core/src/dice.rs
@@ -107,3 +107,129 @@ pub fn roll_many(request: RollManyRequest) -> Vec<usize> {
         .map(|_| roll_die(request.num_faces))
         .collect()
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const NUM_TEST_TRIALS: usize = 10000;
+
+    fn assert_range(num_faces: usize, result: usize) {
+        assert!((1..=num_faces).contains(&result));
+    }
+
+    #[test]
+    fn roll_1_d0() {
+        assert_eq!(roll_die(0), 0);
+    }
+
+    #[test]
+    fn roll_1_d6() {
+        let num_faces = 6;
+        for _ in 0..NUM_TEST_TRIALS {
+            assert_range(num_faces, roll_die(num_faces));
+        }
+    }
+
+    #[test]
+    fn roll_1_d20() {
+        let num_faces = 20;
+        for _ in 0..NUM_TEST_TRIALS {
+            assert_range(num_faces, roll_die(num_faces));
+        }
+    }
+
+    #[test]
+    fn parse_valid_roll_many_strs() {
+        let assert_valid = |in_str: &str, exp_ndice: usize, exp_nfaces: usize| {
+            assert_eq!(
+                parse_roll_many_str(in_str).unwrap(),
+                RollManyRequest {
+                    num_dice: exp_ndice,
+                    num_faces: exp_nfaces,
+                }
+            );
+        };
+
+        assert_valid("1d6", 1, 6);
+        assert_valid("5d20", 5, 20);
+        assert_valid("0d0", 0, 0);
+        assert_valid("999d999", 999, 999);
+    }
+
+    #[test]
+    fn parse_invalid_roll_many_strs() {
+        let assert_invalid = |in_str: &str| {
+            let _ = parse_roll_many_str(in_str).unwrap_err();
+        };
+
+        assert_invalid("1D7");
+        assert_invalid("1 d6");
+        assert_invalid("oned6");
+        assert_invalid("-1d6");
+        assert_invalid("1.0d6");
+        assert_invalid("999d 8");
+        assert_invalid("1d325A");
+        assert_invalid("1");
+    }
+
+    #[test]
+    fn roll_many_d20s() {
+        let num_dice = NUM_TEST_TRIALS;
+        let num_faces = 20;
+        let req = RollManyRequest {
+            num_dice,
+            num_faces,
+        };
+
+        let results = roll_many(req);
+        assert_eq!(results.len(), num_dice);
+        for result in results {
+            assert!((1..=num_faces).contains(&result));
+        }
+    }
+
+    #[test]
+    fn roll_single() {
+        let num_dice = 1;
+        let num_faces = 6;
+        let req = RollManyRequest {
+            num_dice,
+            num_faces,
+        };
+
+        let results = roll_many(req);
+        assert_eq!(results.len(), 1);
+        assert!((1..=num_faces).contains(&results[0]));
+    }
+
+    #[test]
+    fn roll_none() {
+        let num_dice = 0;
+        let num_faces = 123456;
+        let req = RollManyRequest {
+            num_dice,
+            num_faces,
+        };
+
+        let results = roll_many(req);
+        assert!(results.is_empty());
+    }
+
+    #[test]
+    fn roll_many_d20s_from_str() {
+        let in_str = "42d20";
+        let num_dice = 42;
+        let num_faces = 20;
+        let exp_req = RollManyRequest {
+            num_dice,
+            num_faces,
+        };
+        let result = roll_many_from_str(in_str).unwrap();
+        assert_eq!(exp_req, result.request);
+        assert_eq!(result.results.len(), num_dice);
+        for roll in result.results {
+            assert!((1..=num_faces).contains(&roll));
+        }
+    }
+}
